33a1ad69194cbe8ef9158caad38e1fbd148cedf3
Mrak-IW <mrak.iw@gmail.com>
2016-01-24 23:58:40 +0200

    Первая домашка закончена в первом приближении.

--- HomeWork/HomeWork1/Program.cs
+++ HomeWork/HomeWork1/Program.cs
@@ -9,6 +9,77 @@ namespace HomeWork1
 	{
 		static void Main(string[] args)
 		{
+			Random rnd = new Random();
+			int[] arr = new int[20];
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = rnd.Next(9);
+			}
+			Console.WriteLine(ConvertToString(arr));
+			QuickSort(arr);
+			Console.WriteLine(ConvertToString(arr));
+		}
+
+		static void QuickSort(int[] arr, int startIndex = 0, int endIndex = -1)
+		{
+			if (endIndex < 0)
+			{
+				endIndex = (int)arr.Length - 1;
+			}
+
+			if (startIndex >= endIndex)
+			{
+				return;
+			}
+
+			int baseIndex = Split(arr, startIndex, endIndex);
+
+			if (baseIndex - startIndex >= 1)
+			{
+				QuickSort(arr, startIndex, baseIndex);
+			}
+
+			if (endIndex - (baseIndex + 1) >= 1)
+			{
+				QuickSort(arr, baseIndex + 1, endIndex);
+			}
+		}
+
+		static int Split(int[] arr, int startIndex, int endIndex)
+		{
+			int i, j;
+			int buf;
+			int baseElem = arr[(endIndex + startIndex) / 2];
+
+			for (i = startIndex, j = endIndex; ; i++, j--)
+			{
+				while (arr[i] < baseElem)
+				{
+					i++;
+				}
+				while (arr[j] > baseElem)
+				{
+					j--;
+				}
+				if (i < j)
+				{
+					buf = arr[i];
+					arr[i] = arr[j];
+					arr[j] = buf;
+				}
+				else return j;
+			}
+			
+		}
+
+		static string ConvertToString(int[] arr)
+		{
+			string res = "";
+			for (int i = 0; i < arr.Length; i++)
+			{
+				res = res + arr[i] + " ";
+			}
+			return res;
 		}
 	}
 }
