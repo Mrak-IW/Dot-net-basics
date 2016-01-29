d85e2c7f1af82a18b469d38a63efe2f9a8c11af2
Mrak-IW <mrak.iw@gmail.com>
2016-01-29 17:05:10 +0200

    Начата практика №5

---
+++ Labs/Lab5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+
+			Console.Write("Введите номер задания:\t");
+			int taskNum = Int32.Parse(Console.ReadLine());
+			switch (taskNum)
+			{
+				default:
+					Console.WriteLine("Задание с таким номером не реализовано");
+					break;
+				case 1:
+					Console.WriteLine("Задание {0}:", taskNum);
+					{
+						
+					}
+					break;
+			}
+		}
+	}
+}
