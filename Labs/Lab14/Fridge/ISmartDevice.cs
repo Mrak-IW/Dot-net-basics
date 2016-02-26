26789db2637353c01ed0c01d5938c6067c5e4e37
Mrak-IW <mrak.iw@gmail.com>
2016-02-26 19:40:35 +0200

    Подготовка к практике 14

---
+++ Labs/Lab14/Fridge/ISmartDevice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab14
+{
+	public interface ISmartDevice
+	{
+		string Name { get; }
+		EPowerState DeviceState { get; set; }
+		ISmartHouse Parent { get; set; }
+
+		void On();
+		void Off();
+		void Break();
+	}
+}
---
+++ Labs/Lab14/Fridge/SmartDevice.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab14
+{
+	public abstract class SmartDevice : ISmartDevice
+	{
+		private string name;
+		private EPowerState state;
+		private static uint nextID = 0;
+		private ISmartHouse parent;
+
+		public SmartDevice(string name)
+		{
+			this.Name = name;
+		}
+
+		public virtual string Name
+		{
+			get
+			{
+				return name;
+			}
+			protected set
+			{
+				name = value;
+			}
+		}
+
+		public virtual EPowerState DeviceState
+		{
+			get
+			{
+				return state;
+			}
+
+			set
+			{
+				state = value;
+			}
+		}
+
+		public virtual ISmartHouse Parent
+		{
+			get
+			{
+				return parent;
+			}
+
+			set
+			{
+				parent = value;
+			}
+		}
+
+		public virtual void Break()
+		{
+			DeviceState = EPowerState.Broken;
+		}
+
+		public virtual void Off()
+		{
+			if (DeviceState != EPowerState.Broken)
+			{
+				DeviceState = EPowerState.Off;
+			}
+		}
+
+		public virtual void On()
+		{
+			if (DeviceState != EPowerState.Broken)
+			{
+				DeviceState = EPowerState.On;
+			}
+		}
+	}
+}
