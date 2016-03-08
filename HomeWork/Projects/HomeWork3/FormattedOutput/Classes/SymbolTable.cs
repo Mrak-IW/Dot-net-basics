64333a065eac484f06a4aa15fab17d7ab3a1d9ab
Mrak-IW <mrak.iw@gmail.com>
2016-03-08 21:21:56 +0200

    Подчищен ненужный код и комменты

--- HomeWork/Projects/HomeWork3/FormattedOutput/Classes/SymbolTable.cs
+++ HomeWork/Projects/HomeWork3/FormattedOutput/Classes/SymbolTable.cs
@@ -32,13 +32,6 @@ namespace HomeWork3.FormattedOutput.Classes
 			char FrameCornerDR = (char)0x255D;
 
 			int i;
-			//for (int i = 0; i < setOfEntries.Count; i++)
-			//{
-			//    if (setOfEntries[i] is ISymbolTableReady)
-			//    {
-			//        result = string.Format("{0}\n{1}", result, setOfEntries[i]);
-			//    }
-			//}
 
 			//Формирование массива на выход
 			List<string[]> lst = new List<string[]>();
@@ -90,9 +83,6 @@ namespace HomeWork3.FormattedOutput.Classes
 			upperFrame = string.Concat(upperFrame, new string(FrameHor2, width[i] + 2), FrameCornerUR);
 			middleFrame = string.Concat(middleFrame, new string(FrameHor1, width[i] + 2), FrameTRight);
 			lowerFrame = string.Concat(lowerFrame, new string(FrameHor2, width[i] + 2), FrameCornerDR);
-			//Console.WriteLine(upperFrame);
-			//Console.WriteLine(middleFrame);
-			//Console.WriteLine(lowerFrame);
 
 			//Набираем таблицу
 			string buf;
@@ -135,61 +125,6 @@ namespace HomeWork3.FormattedOutput.Classes
 
 			Console.WriteLine(result);
 			return result;
-		}
-
-		private void Dummy()
-		{
-			/*
-			//Находим ширину таблицы
-			int width = 0;
-			foreach (string s in lst)
-			{
-				if (s.Length > width)
-				{
-					width = s.Length;
-				}
-			}
-
-			int widthReady = 0;
-			foreach (string s in lstReady)
-			{
-				if (s.Length > widthReady)
-				{
-					widthReady = s.Length;
-				}
-			}
-
-			//Печатаем таблицу
-			char horFrame2 = (char)0x2550;
-			char horFrame1 = (char)0x2500;
-			char verFrame2 = (char)0x2551;
-			char tFrame = (char)0x2566;
-			char tUpFrame = (char)0x2569;
-			char tRightFrame = (char)0x255F;
-			char tLeftFrame = (char)0x2562;
-			char crossFrame = (char)0x256B;
-			char ulCorner = (char)0x2554;
-			char urCorner = (char)0x2557;
-			char dlCorner = (char)0x255A;
-			char drCorner = (char)0x255D;
-
-			string upperFrame = string.Concat(ulCorner, new string(horFrame2, width + 2), tFrame, new string(horFrame2, widthReady + 2), urCorner);
-			string lowerFrame = string.Concat(dlCorner, new string(horFrame2, width + 2), tUpFrame, new string(horFrame2, widthReady + 2), drCorner);
-			string middleFrame = string.Concat(tRightFrame, new string(horFrame1, width + 2), crossFrame, new string(horFrame1, widthReady + 2), tLeftFrame);
-
-			Console.WriteLine(upperFrame);
-			for (i = 0; i < lst.Count; i++)
-			{
-				ans = string.Format("{0} {1}{2} {0} {3}{4} {0}",
-					verFrame2,
-					lst[i],
-					new string(' ', width - lst[i].Length),
-					lstReady[i],
-					new string(' ', widthReady - lstReady[i].Length));
-				Console.WriteLine(ans);
-				Console.WriteLine(i < lst.Count - 1 ? middleFrame : lowerFrame);
-			}
-			*/
 		}
 	}
 }
